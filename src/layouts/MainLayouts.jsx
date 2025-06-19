410058611140a1a2c071bc72b01675f902012de8
Maseyitov <masejitovandaniel@gmail.com>
2025-06-19 12:37:12 +0600

    footer

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 const MainLayouts = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayouts = () => {
       <div>
         <Outlet/>
       </div>
+      <Footer/>
     </div>
   )
 }
