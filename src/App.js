ae6a92648bdbeec13a9c9cb36476a2111e58ab67
Tagaev744 <tagaevbeksultan139@gmail.com>
2025-06-24 16:58:33 +0600

    finish Services

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import FAQ from './components/FAQ';
-import Lines from './components/Lines';
-import Services from './components/Services';
 import MainRoutes from './routes/MainRoutes';
 
 function App() {
   return (
     <div className="App">
-     <Services/>
-      <FAQ/>
-       <Lines/>
        <MainRoutes/>
     </div>
   );
